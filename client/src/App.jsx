98a54fb6b88e3321d9be3c793729e6c540dc04ad
danielpolov <danielpolowow@gmail.com>
2024-02-17 21:03:56 -0500

    Added keyboard event listener

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import DisplayTimer from './Components/DisplayTimer';
-import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <>  
       <DisplayTimer />
-      <Button variant='dark'>Dark</Button>{' '}
     </>
   )
 }
--- client/src/Components/DisplayTimer.jsx
+++ client/src/Components/DisplayTimer.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+
+let defaultTimerValues = {
+    started: false,
+    minutes: 0,
+    seconds: 0,
+    miliseconds: 0
+}
 
 const DisplayTimer = () =>{
+    const [timerValues, setValues] = useState(defaultTimerValues);
+
+    const toggleStopStartTimer = () =>{
+        timerValues.started ? setValues({...timerValues, started: false}) : 
+        setValues({...timerValues, started: true});
+        console.log(timerValues);
+    }
+    
+    useEffect(() => {
+        const keyDownHandler = event => {
+            console.log('User pressed: ', event.key);
+
+            if (event.keyCode === 32) {
+                event.preventDefault();
+
+                toggleStopStartTimer();
+            }
+        };
+
+        document.addEventListener('keydown', keyDownHandler);
+
+        return () => {
+            document.removeEventListener('keydown', keyDownHandler);
+        };
+    }, [timerValues, toggleStopStartTimer]);
+
     return <div className='display-timer'>
-        00,00
+        {timerValues.seconds},{timerValues.miliseconds}
+        <br />
+        <Button variant='dark' onClick={toggleStopStartTimer}>Dark</Button>{' '}
     </div>
 }
 
